7150552f3a52cba4aac17bba4122501f577e6c91
Ileis <gabrielileis07@gmail.com>
2025-11-12 23:25:58 -0300

    insert random numbers [0-50]

--- BPlus Tree/main.py
+++ BPlus Tree/main.py
@@ -1,36 +1,23 @@
 from b_plus_tree import BPlusTree
+import random
 
 def main() -> None:
+    random.seed(None)
     bptree = BPlusTree(4)
 
-    keys_to_insert = [10, 20, 5, 15, 25, 30, 7, 12, 40, 50, 60, 35]
-    for key in keys_to_insert:
-        print(f"Inserindo {key}...")
-        bptree.insert(key, f"valor_{key}")
+    l = list(range(51))
+    for i in range(len(l) // 2):
+        j = random.randint(0, len(l) - 1)
+        aux = l[i]
+        l[i] = l[j]
+        l[j] = aux
+
+    for i in l:
+        print(f'inserting: {i}')
+        bptree.insert(i, f'value: {i}')
         bptree.print_tree()
-        print("-" * 20)
+        print('-' * 50)
 
-    print()
-    print("--- Lista das Folhas ---")
-    bptree.print_leaves()
-
-    print()
-    print("--- Buscando elementos ---")
-    print(f"Busca por 15: {bptree.search(15)}")
-    print(f"Busca por 99: {bptree.search(99)}")
-
-    print()
-    print("--- Removendo elementos ---")
-    keys_to_remove = [15, 25, 50]
-    for key in keys_to_remove:
-        print(f"Removendo {key}...")
-        bptree.remove(key)
-        bptree.print_tree()
-        print("-" * 20)
-
-    print("\n--- Árvore Final ---")
-    bptree.print_leaves()
-    bptree.print_tree()
 
 if __name__ == "__main__":
     main()
